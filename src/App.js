e09f41eff871f1e4ddcf436fe92ab3b4235cc3d5
mumukshapant <mumukshapan97@gmail.com>
2023-12-04 23:50:01 -0500

    new s

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
